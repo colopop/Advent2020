dc818ec0d0c1c7dfd751c9aa3e280d6cfbc19e8d
colopop <seberg94@gmail.com>
2020-12-07 23:35:19 -0500

    Optimized day 1 puzzle 1.

--- 1/a.py
+++ 1/a.py
@@ -1,12 +1,7 @@
 with open("inp.txt") as inp:
-   nums = [int(l) for l in inp.readlines()]
+   nums = set([int(l) for l in inp.readlines()])
 
-nums.sort()
-lower = 0
-upper = len(nums) - 1
-while nums[lower] + nums[upper] != 2020:
-   if nums[lower] + nums[upper] < 2020:
-      lower += 1
-   else:
-      upper -= 1
-print(lower, upper, nums[lower], nums[upper], nums[lower] * nums[upper])
+for num in nums:
+    if (2020 - num) in nums:
+        print(num * (2020 - num))
+        break
