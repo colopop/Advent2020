61757f3c870c48d8efefeaa3d2bcb736cb9d8ded
colopop <seberg94@gmail.com>
2020-12-14 12:21:54 -0500

    Added a proper solution for day 13 puzzle 2.

--- 13/b.py
+++ 13/b.py
@@ -1,7 +1,39 @@
-buses = [29,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',41,'x','x','x','x','x','x','x','x','x',601,'x','x','x','x','x','x','x',23,'x','x','x','x',13,'x','x','x',17,'x',19,'x','x','x','x','x','x','x','x','x','x','x',463,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',37]
+with open("input.txt") as inp:
+    earliest = int(inp.readline())
+    buses = [b for b in inp.readline().split(',')]
 
-#what we want is
-#t such that
+#to calculate the inverse of x modulo n
+def mod_inverse(x, n):
+    def ext_euclid(a, b, p0, p1, n):
+        q = a // b
+        r = a % b
+        if r == 0:
+            if b != 1:
+                raise ValueError("Value is not invertible.")
+            return p1
+        else:
+            p_next = (p0 - p1*q) % n
+            return ext_euclid(b, r, p1, p_next, n)
+
+    if x > n:
+        x = x % n
+    return ext_euclid(n, x, 0, 1, n)
+
+#solve an instance of the Chinese Remainder Theorem
+#constraints must be of the form [(residue1, mod1), (residue2, mod2), ...]
+def solve_CRT(constraints):
+    if len(constraints) == 0:
+        return 0
+    elif len(constraints) == 1:
+        return constraints[0][0] % constraints[0][1]
+    else:
+        mod_prod = 1
+        for c in constraints:
+            mod_prod *= c[1]
+
+        return sum(residue * (mod_prod//mod) * mod_inverse(mod_prod//mod, mod) for residue,mod in constraints) % mod_prod
+
+#what we want is t such that
 #t % 29 == 0
 #(t+19) % 41 == 0
 #(t+29) % 601 == 0
@@ -22,26 +54,4 @@ buses = [29,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',
 #t % 463 == -60
 #t % 37 == -97
 #a.k.a. Chinese remainder theorem
-
-def check_validity(timestamp, buses):
-    for i, bus in enumerate(buses):
-        if bus == 'x':
-            continue
-        else:
-            if bus - (timestamp % bus) != i:
-                return False
-    return True
-
-# timestamp = 0
-# for i, bus in enumerate(buses[1:]):
-#     if bus == 'x': continue
-#     timestamp *= bus
-#     timestamp += i
-#     if check_validity(timestamp, buses):
-#         break
-
-for i,bus in enumerate(buses):
-    if bus != 'x':
-        print(bus-i,bus)
-
-print(timestamp)
+print(solve_CRT([(-1*index, int(bus_val)) for index,bus_val in enumerate(buses) if bus_val.isdigit()]))
