affef14dd2dccdde69b4222712c15eadee1a43e1
colopop <seberg94@gmail.com>
2020-12-15 00:36:41 -0500

    Removed debugging line.

--- 15/b.py
+++ 15/b.py
@@ -7,7 +7,6 @@ ages = {num : idx for idx,num in enumerate(ages)}
 
 for i in range(6,30000000):
     old_seed = seed
-   # print(seed)
     if seed in ages:
         new_seed = i - ages[seed]
         ages[seed] = i
